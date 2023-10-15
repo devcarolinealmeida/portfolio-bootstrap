fe091f05b36816e83246f08a887da4735a69cc1a
devcarolinealmeida <carolineitalia85@gmail.com>
2023-10-15 15:40:23 +0200

    add section intro

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+/* scroller */
+window.addEventListener("scroll", setScrollVar);
+window.addEventListener("resize", setScrollVar);
+
+function setScrollVar() {
+    const htmlElement = document.documentElement;
+    const percentOfScreenHeightScrolled = htmlElement.scrollTop / htmlElement.clientHeight;
+    htmlElement.style.setProperty("--scroll", Math.min(percentOfScreenHeightScrolled * 100, 100));
+}
+
+setScrollVar()
+
+const observer = new IntersectionObserver(entries => {
+    for(let i = entries.length - 1; i >= 0; i--) {
+        const entry = entries[i]
+        if(entry.isIntersecting) {
+            document.querySelectorAll("[data-img]").forEach(img => {
+                img.classList.remove("show")
+            })
+            const img = document.querySelector(entry.target.dataset.imgToShow);
+            img?.classList.add("show");
+            break
+        }
+    }
+})
+
+document.querySelectorAll("[data-img-to-show]").forEach(section => {
+    observer.observe(section)
+})
+
+
+/* Auto typed */
+var typed = new Typed('.auto-type', {
+    strings: ['Caroline.', 'a Developer.', 'Freelancer.'],
+    typeSpeed: 130,
+    backSpeed: 100,
+    loop: true
+  });
+
+  
